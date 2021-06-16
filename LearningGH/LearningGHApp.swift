44e277e845c7ed89b9973fdc956ad90c6d271dd7
AlchemyLab <developer@studioalchemy.net>
2021-06-16 08:08:38 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Luigi Pesce on 16/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
